f232bfa96c967945e6a7d0b278c2bc85ca0ca1bb
Hakintoshi <morzhov2020@mail.ru>
2025-08-29 10:18:37 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <h1>{{ tableData }}</h1>
+  </div>
+</template>
+
+<script>
+import { tableData } from './tableData'
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+        tableData,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
